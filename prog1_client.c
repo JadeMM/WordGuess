d64e897302b6d0e540a37fe73c35503afbac679e
barrets9 <barrets9@wwu.edu>
2018-10-12 14:50:06 -0700

    Sorted includes

--- prog1_client.c
+++ prog1_client.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int main( int argc, char **argv) {
